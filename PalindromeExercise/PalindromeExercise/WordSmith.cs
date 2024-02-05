dd858688e0c41e0019653b3b8d5cbc51c3ab8b39
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-02-05 13:28:31 -0600

    Finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            char[] splitString = word.ToCharArray();
+            char[] compare = new char[splitString.Length];
+            int count = 0;
+            for(int i = splitString.Length - 1; i >= 0; i--)
+            {
+                compare[count] = splitString[i];
+                count++;
+            }
+            for(int i = 0; i < splitString.Length; i++)
+            {
+                if (splitString[i] != compare[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
+using PalindromeExercise;
 using System;
+using System.Diagnostics.SymbolStore;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("radar", true)]
+        [InlineData("level", true)]
+        [InlineData("hello", false)]
+        [InlineData("madam", true)]
+        [InlineData("racecars", false)]
+        public void PalindromeTest(string input, bool expected)
         {
-
+            WordSmith pal = new WordSmith();
+            bool actual = pal.IsAPalindrome(input);
+            Assert.Equal(expected, actual);
         }
     }
 }
